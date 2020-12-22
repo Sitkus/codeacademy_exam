2c9c26e940d1c367083597ab98dc6ba36ad24efc
Sitkus <65811246+Sitkus@users.noreply.github.com>
2020-12-22 14:29:00 +0200

    Fixed an iFrame issue with google map.

--- app/templates/content/index.tpl.php
+++ app/templates/content/index.tpl.php
@@ -25,5 +25,5 @@
 </article>
 
 <section class="map">
-    <iframe class="map__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=Saul%C4%97tekio%20al.%2015+(Code%20Academy)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    <iframe class="map__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=Vilnius,%20Saul%C4%97tekio%20al.%2015+(Code%20Academy)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
 </section>

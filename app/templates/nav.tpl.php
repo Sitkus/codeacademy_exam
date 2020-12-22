6a005020d9bedcfb9162565d3460c3109020a42c
Sitkus <65811246+Sitkus@users.noreply.github.com>
2020-12-22 19:51:30 +0200

    Final changes made. Slight change to navigation comparisson operator.

--- app/templates/nav.tpl.php
+++ app/templates/nav.tpl.php
@@ -4,14 +4,14 @@
             <div class="nav__side">
                 <?php foreach ($data['left'] as $link => $title): ?>
                     <li class="nav__item">
-                        <a href="<?php print $link; ?>" class="nav__link<?php $link == $_SERVER['REQUEST_URI'] ?  print '  nav__link--active' : null ?>"><?php print $title; ?></a>
+                        <a href="<?php print $link; ?>" class="nav__link<?php $link === $_SERVER['REQUEST_URI'] ?  print '  nav__link--active' : null ?>"><?php print $title; ?></a>
                     </li>
                 <?php endforeach; ?>
             </div>
             <div class="nav__side">
                 <?php foreach ($data['right'] as $link => $title): ?>
                     <li class="nav__item">
-                        <a href="<?php print $link; ?>" class="nav__link<?php $link == $_SERVER['REQUEST_URI'] ?  print '  nav__link--active' : null ?>"><?php print $title; ?></a>
+                        <a href="<?php print $link; ?>" class="nav__link<?php $link === $_SERVER['REQUEST_URI'] ?  print '  nav__link--active' : null ?>"><?php print $title; ?></a>
                     </li>
                 <?php endforeach; ?>
             </div>
